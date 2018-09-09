832b06efcb42e4a965850248127506701e20d191
Ted M. Young <tedyoung@gmail.com>
2018-09-09 10:04:00 -0700

    Initial Kitchen Kiosk display application

---
+++ src/main/java/com/welltestedlearning/kitchenkiosk/KitchenKioskApplication.java
@@ -0,0 +1,12 @@
+package com.welltestedlearning.kitchenkiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenKioskApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KitchenKioskApplication.class, args);
+  }
+}
