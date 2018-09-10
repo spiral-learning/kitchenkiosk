e90460952e342eb0950a7113885018223bed4620
Ted M. Young <tedyoung@gmail.com>
2018-09-09 22:30:05 -0500

    Quick controller to return date/time page at root Update library versions

---
+++ src/main/java/com/welltestedlearning/kitchenkiosk/KioskOrderDisplayController.java
@@ -0,0 +1,13 @@
+package com.welltestedlearning.kitchenkiosk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class KioskOrderDisplayController {
+
+  @GetMapping("/")
+  public String orderDisplay() {
+    return "order-display";
+  }
+}
